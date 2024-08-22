97faf89afdcdbc38ffa17d79744fe6a0ab639e15
Roman Zanko <roman.zanko.vl@gmail.com>
2024-08-22 09:34:12 +0400

    Added reading from stdin to grep-lite

--- src/ch2/grep-lite/src/main.rs
+++ src/ch2/grep-lite/src/main.rs
@@ -4,6 +4,16 @@ use std::io::prelude::*;
 use regex::Regex;
 use clap::{App, Arg};
 
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => ()
+        }
+    }
+}
+
 fn main() {
     let args = App::new("grep-lite")
         .version("0.1")
@@ -15,21 +25,21 @@ fn main() {
         .arg(Arg::with_name("input")
                   .help("File to search")
                   .takes_value(true)
-                  .required(true))
+                  .required(false))
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let input = args.value_of("input").unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
+    let input = args.value_of("input").unwrap_or("-");
 
-    for line_ in reader.lines() {
-        let line = line_.unwrap();
-        match re.find(&line) {
-            Some(_) => println!("{}", line),
-            None => ()
-        }
+    if input == "-" {
+        let stdin = std::io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
