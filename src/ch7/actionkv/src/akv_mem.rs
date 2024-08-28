ab4cf2bc8100edc11ca4958f1fea40a50f9e5e2a
Roman Zanko <roman.zanko.vl@gmail.com>
2024-08-28 08:27:29 +0400

    Completed v1 of key-value store project

--- src/ch7/actionkv/src/akv_mem.rs
+++ src/ch7/actionkv/src/akv_mem.rs
@@ -29,4 +29,21 @@ fn main() {
     let mut store = ActionKV::open(path).expect("unable to open file");
 
     store.load().expect("unable to load data");
+
+    match action {
+        "get" => match store.get(key).unwrap() {
+            None => eprintln!("{:?} not found", key),
+            Some(value) => println!("{:?}", value),
+        },
+        "delete" => store.delete(key).unwrap(),
+        "insert" => {
+            let value = maybe_value.expect(&USAGE).as_ref();
+            store.insert(key, value).unwrap();
+        },
+        "update" => {
+            let value = maybe_value.expect(&USAGE).as_ref();
+            store.update(key, value).unwrap();
+        }
+        _ => eprintln!("{}", action),
+    }
 }
