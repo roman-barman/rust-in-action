1ad04d5b9d08813824089db99e4d1fd3eb77780f
Roman Zanko <roman.zanko.vl@gmail.com>
2024-08-28 08:58:24 +0400

    Added v2 of key-value store project

--- src/ch7/actionkv/src/akv_disk.rs
+++ src/ch7/actionkv/src/akv_disk.rs
@@ -1,4 +1,5 @@
 use libactionkv::ActionKV;
+use std::collections::HashMap;
 
 #[cfg(target_os = "windows")]
 const USAGE: &str = "
@@ -18,7 +19,19 @@ Usage:
     akv_mem FILE update KEY VALUE
 ";
 
+type ByteString = Vec<u8>;
+type ByteStr = [u8];
+
+fn store_index_on_disk(a: &mut ActionKV, index_key: &ByteStr) {
+    a.index.remove(index_key);
+    let index_as_bytes = bincode::serialize(&a.index).unwrap();
+    a.index = HashMap::new();
+    a.insert(index_key, &index_as_bytes).unwrap()
+}
+
 fn main() {
+    const INDEX_KEY: &ByteStr = b"+index";
+
     let args: Vec<String> = std::env::args().collect();
     let fname = args.get(1).expect(&USAGE);
     let action = args.get(2).expect(&USAGE).as_ref();
@@ -31,18 +44,30 @@ fn main() {
     store.load().expect("unable to load data");
 
     match action {
-        "get" => match store.get(key).unwrap() {
-            None => eprintln!("{:?} not found", key),
-            Some(value) => println!("{:?}", value),
+        "get" => {
+            let index_as_bytes = store.get(&INDEX_KEY).unwrap().unwrap();
+            let index_decode = bincode::deserialize(&index_as_bytes);
+
+            let index: HashMap<ByteString, u64> = index_decode.unwrap();
+
+            match index.get(key) {
+                None => eprintln!("{:?} not found", key),
+                Some(&i) => {
+                    let kv = store.get_at(i).unwrap();
+                    println!("{:?}", kv.value);
+                }
+            }
         },
         "delete" => store.delete(key).unwrap(),
         "insert" => {
             let value = maybe_value.expect(&USAGE).as_ref();
             store.insert(key, value).unwrap();
+            store_index_on_disk(&mut store, &INDEX_KEY);
         },
         "update" => {
             let value = maybe_value.expect(&USAGE).as_ref();
             store.update(key, value).unwrap();
+            store_index_on_disk(&mut store, &INDEX_KEY);
         }
         _ => eprintln!("{}", action),
     }
